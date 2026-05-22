8d80474851cc4bf7e31d803b00784ec5c3c6cb22
Zayden <zaylanthier-lang>
2026-05-22 17:33:58 -0400

    Finished Code

--- server/models.py
+++ server/models.py
@@ -17,6 +17,14 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship(
+        "Review",
+        back_populates="customer",
+        cascade="all, delete-orphan"
+    )
+
+    items = association_proxy("reviews", "item")
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
@@ -28,5 +36,79 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship(
+        "Review",
+        back_populates="item",
+        cascade="all, delete-orphan"
+    )
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(
+        db.Integer,
+        db.ForeignKey("customers.id")
+    )
+
+    item_id = db.Column(
+        db.Integer,
+        db.ForeignKey("items.id")
+    )
+
+    customer = db.relationship(
+        "Customer",
+        back_populates="reviews"
+    )
+
+    item = db.relationship(
+        "Item",
+        back_populates="reviews"
+    )
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
+
+
+class CustomerSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+    reviews = fields.Nested(
+        "ReviewSchema",
+        many=True,
+        exclude=("customer",)
+    )
+
+
+class ItemSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    price = fields.Float()
+
+    reviews = fields.Nested(
+        "ReviewSchema",
+        many=True,
+        exclude=("item",)
+    )
+
+
+class ReviewSchema(Schema):
+    id = fields.Int()
+    comment = fields.Str()
+
+    customer = fields.Nested(
+        "CustomerSchema",
+        exclude=("reviews",)
+    )
+
+    item = fields.Nested(
+        "ItemSchema",
+        exclude=("reviews",)
+    )
